b98b706cdf3f63374702c873c73d06d112582959
SBViside <ohvatuki@gmail.com>
2021-02-13 16:11:35 +0300

    nothing

--- guess_num.py
+++ guess_num.py
@@ -1,14 +1,14 @@
 from random import randint
 
 def comparisons(num, ans):
-	if ans < num:
-		ans = int(input('\nНеверно.. Число больше(>)!\nПовтори попытку: '))
-		return ans
-	if ans > num:
-		ans = int(input('\nНеверно.. Число меньше(<)!\nПовтори попытку: '))
-		return ans
-	else:
-		return None
+    if ans < num:
+        ans = int(input('\nНеверно.. Число больше(>)!\nПовтори попытку: '))
+        return ans
+    if ans > num:
+        ans = int(input('\nНеверно.. Число меньше(<)!\nПовтори попытку: '))
+        return ans
+    else:
+        return None
 
 print('Угадай число от 1 до 100\n')
 input('Нажми ENTER чтобы начать...\n')
@@ -17,10 +17,10 @@ NUMBER = randint(1,100)
 ANSWER = int(input('Введи число: '))
 
 while True:
-	ANSWER = comparisons(NUMBER, ANSWER)
+    ANSWER = comparisons(NUMBER, ANSWER)
 
-	if ANSWER == None:
-		print('\nТы победил!!! Congratulations!\n')
-		break
+    if ANSWER == None:
+        print('\nТы победил!!! Congratulations!\n')
+        break
 
 print('Число было: {}'.format(NUMBER))
--- guess_word.py
+++ guess_word.py
@@ -1,13 +1,13 @@
 from random import randint
 
 def print_word(*wrd, char=None, lst=[]):
-	main_answer = ''
-	for i in wrd:
-		if (char == i) or (i in lst):
-			main_answer += i
-		else:
-			main_answer += '_'
-	return main_answer
+    main_answer = ''
+    for i in wrd:
+        if (char == i) or (i in lst):
+            main_answer += i
+        else:
+            main_answer += '_'
+    return main_answer
 
 words_tuple = ('python', 'computer', 'desktop', 'laptop', 'keyboard', 'mouse', 'internet', 'touchpad', 'windows', 'linux',)
 WORD = words_tuple[randint(0,9)]
@@ -21,29 +21,29 @@ l = 10
 print('\nСлово из {} букв: \n'.format(len(WORD)), print_word(*WORD).replace('', ' ').strip())
 
 while True:
-	if l != 0:
-		char1 = input('\nВведите символ: ')
+    if l != 0:
+        char1 = input('\nВведите символ: ')
 
-		if 0 < len(char1) < 2:
+        if 0 < len(char1) < 2:
 
-			if char1 in WORD:
-				print('\nПравильно!')
+            if char1 in WORD:
+                print('\nПравильно!')
 
-				right_list.append(char1)
-				print(print_word(*WORD, char=char1, lst=right_list).replace('', ' ').strip())
+                right_list.append(char1)
+                print(print_word(*WORD, char=char1, lst=right_list).replace('', ' ').strip())
 
-				if print_word(*WORD, char=char1, lst=right_list) == WORD:
-					print('\nПоздравляю! Ты победил!\n')
-					break
+                if print_word(*WORD, char=char1, lst=right_list) == WORD:
+                    print('\nПоздравляю! Ты победил!\n')
+                    break
 
-			else:
-				l -= 1
-				print('\nНеправильно!\nОсталось {} попыток...\n'.format(l))
-				print(print_word(*WORD, lst=right_list).replace('', ' ').strip())
-		else:
-			print('\nНЕОБХОДИМО ВВЕСТИ СИМВОЛ!!!\nМинус одна попытка.')
-			l -= 1
-	else:
-		print('\nВы проиграли...\nПраильное слово было: {}'.format(WORD))
-		break
+            else:
+                l -= 1
+                print('\nНеправильно!\nОсталось {} попыток...\n'.format(l))
+                print(print_word(*WORD, lst=right_list).replace('', ' ').strip())
+        else:
+            print('\nНЕОБХОДИМО ВВЕСТИ СИМВОЛ!!!\nМинус одна попытка.')
+            l -= 1
+    else:
+        print('\nВы проиграли...\nПраильное слово было: {}'.format(WORD))
+        break
 
